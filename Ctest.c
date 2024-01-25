c8b8a0888327417d69245cb7dc21df7b6b1baa46
Mona941 <sassim94170@gmail.com>
2024-01-25 12:49:20 +0100

    Ajout du ficher .c

---
+++ Ctest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Tableau.h"
+
+/*la taille du tableau est demandée dans le main, et la mémoire est allouée en conséquence. 
+La vérification de l'allocation réussie est effectuée avant d'utiliser le tableau. */
+/*parcourir un tableau chaine caractere*/
+
+int main()
+{
+    printf()
+
+}
